6b69fb00226a1a1c4b1887c55edc236fb22dc88b
Charlotte Morrison <charlottemaemorrison@gmail.com>
2018-09-03 11:23:14 -0400

    Initial Commit

---
+++ src/Tester.java
@@ -0,0 +1,24 @@
+
+public class Tester {
+
+	public static void main(String[] args) {
+		//initialize the variables
+		
+		double dA = 37.9;
+		double dB = 1004.128;
+		int iA = 12;
+		int iB = 18;
+		
+		//your results should print out 
+		//Problem #: (solution) 
+		//to the following problems.
+		
+		//Problem 1: 57.2 * (iA / iB) + 1
+		//Problem 2: 57.2 * ((double)iA / iB) + 1
+		//Problem 3: 15 - iA * (dA * 3) + 4
+		//Problem 4: 15 - iA * (int)(dA * 3) + 4
+		//Problem 5: 15 - iA * ((int)dA * 3) + 4
+		
+	}
+
+}
